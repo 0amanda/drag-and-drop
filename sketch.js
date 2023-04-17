77b33a2032cb58ba15abdcab9f3965bcff1ddb34
0amanda <amand20201@gmail.com>
2023-04-17 17:24:33 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,31 +1,43 @@
 // Click and Drag an object
 
 let shape1;
-let shape2;
+let extraCanvas;
+let fr = 6;
+
 
 function setup() {
   createCanvas(640, 360);
-  shape1 = new Draggable(100, 100, 50, 50);
-  shape2 = new Draggable(150, 100, 50, 50);
+  extraCanvas = createGraphics(640, 360);
+  extraCanvas.clear;
+  shape1 = new Draggable(100, 100, 100, 100);
+  frameRate(fr);
 }
+
 
 function draw() {
   background(255);
   shape1.over();
   shape1.update();
   shape1.show();
-  shape2.over();
-  shape2.update();
-  shape2.show();
+ 
+  if (mouseIsPressed) {
+    extraCanvas.fill(190,0,0,40);
+    extraCanvas.ellipse(mouseX, mouseY, 75);
+  } else {
+    extraCanvas.fill(255,0,0,40);
+  }
+  extraCanvas.noStroke();
+extraCanvas.translate();
+  extraCanvas.ellipse(mouseX, mouseY, 50);
+
+   image(extraCanvas,0,0);
 
 }
 
 function mousePressed() {
   shape1.pressed();
-  shape2.pressed();
 }
 
 function mouseReleased() {
   shape1.released();
-  shape2.released();
 }
